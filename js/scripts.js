64749e1a99817eafe08804adcd5d4cd0b98edb2a
hannah melendy and marina poltorak <marina@>
2019-06-04 14:39:23 -0700

    add buttons to change background color

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,25 @@ $(document).ready(function() {
     $(".excitedFish").toggle();
     $(".chillFish").toggle();
   });
+
+
+  $("button#goldenrod").click(function() {
+    $("body").removeClass();
+    $("body").addClass("goldenrod-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#lavender").click(function() {
+    $("body").removeClass();
+    $("body").addClass("lavender-background");
+  });
 });
